7b4a1471d76ac69652b26656ff085a6841f30763
Roshan Yadav <roshan.yadav2019@vitbhopal.ac.in>
2021-12-15 21:00:29 +0530

    2nd

---
+++ Chef_and_Pal.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+        cin>>n;
+
+        if(n%2!=0)
+           cout<<"-1"<<endl;
+    
+        else{
+            for(int i=0; i<n/2; i++){
+            cout<<"1";}
+            
+            for(int i=n/2; i<n; i++){
+            cout<<"0";}
+            cout<<endl;
+        }
+	}
+	return 0;
+}
