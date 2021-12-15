7ab0392e6b470baba03afe9a7ce78072e2e9e021
Roshan Yadav <roshan.yadav2019@vitbhopal.ac.in>
2021-12-15 20:35:23 +0530

    starters 20

---
+++ Starters 20/genes.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    char a,b;
+    cin>>a>>b;
+
+    if(a=='R' || b=='R')
+       cout<<"R"<<endl;
+    
+    else if(a=='B' || b=='B')
+       cout<<"B"<<endl;
+
+    else 
+       cout<<"G"<<endl;
+}
