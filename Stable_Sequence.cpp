41a225727559a8b1f613f05e887b2fe671dedab2
Roshan Yadav <roshan.yadav2019@vitbhopal.ac.in>
2021-12-16 10:48:37 +0530

    SS

--- Stable_Sequence.cpp
+++ Stable_Sequence.cpp
@@ -5,7 +5,29 @@ int main() {
 	int t;
 	cin>>t;
 	while(t--){
-	    
+	    int n, count=0;
+        cin>>n;
+
+        vector<int> v;
+
+        for(int i=0; i<n; i++){
+            int a;
+            cin>>a;
+            v.push_back(a);
+        }
+
+        sort(v.begin(), v.end());
+
+        for(int i=0; i<n; i++){
+            if(v[n-1]!=v[i])
+              count++;
+        }
+        
+        if(count>0)
+           cout<<count<<endl;
+        
+        else
+           cout<<"0"<<endl;
 	}
 	return 0;
 }
