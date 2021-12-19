b25c8ba24e260e0669677388d2e5666230e86d37
Roshan Yadav <roshan.yadav2019@vitbhopal.ac.in>
2021-12-19 12:46:59 +0530

    Hackerearth

--- Code Monk/inversions.cpp
+++ Code Monk/inversions.cpp
@@ -5,36 +5,62 @@
 using namespace std;
 
 int main(){
-      int t=1;
+      int t;
       cin>>t;
       
       while(t--){
-          int n=0, count=0;
+          int n, count=0, p=0;
+          bool b= true;
           cin>>n;
           
-          vector<vector<int>> v;
+          vector<vector<int>> v  ;
+         // vector<int> vv;
 
           for(int i=0; i<n; i++){
-                 
+                 vector<int> temp;
               for(int j=0; j<n; j++){
                  
                  int a;
                  cin>>a;
-                 //cout<<a;
-                 v[i][j]==a; 
+                 temp.push_back(a);
+
               }
+
+              v.push_back(temp);
           }
+
 
           for(int i=0; i<n; i++){
 
-              for(int j=0; j<v[i].size(); j++){
+              for(int j=0; j<n; j++){
 
-                int a=v[i][j];
-                cout<<a;
-              
+                 b=true;
+
+                  for(int k=i; k<n; k++){
+                      
+                      if(b = false)
+                         p=0;
+
+                      else 
+                         p=j;
+                        
+                      for(int l=p; l<n; l++){
+
+                          if( v[i][j] > v[k][l] ){
+                             count++;
+                           //  vv.push_back(v[i][j]);
+                             }
+                      }
+
+                      b=false;
+                  }
               }
           }
-
-
+        /*  
+          for(int i=0; i<vv.size(); i++){
+              cout<<vv[i];
+          }
+          cout<<endl;*/
+          cout<< count<<endl;
       }
 }
