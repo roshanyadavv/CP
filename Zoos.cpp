d58c3c948b7681f1efe1b4714d50866c961b84a8
Roshan Yadav <roshan.yadav2019@vitbhopal.ac.in>
2022-02-03 17:33:15 +0530

    cp

--- Zoos.cpp
+++ Zoos.cpp
@@ -4,25 +4,25 @@ using namespace std;
 
 int main(){
 
-    int t, min=0, count=0;
+    int t, x=0, y=0;
+    char a;
   //  cin>>t;
     
-  //  while(t--){
+  //  while(t--)
+  string s;
+  cin>>s;
 
-        string s;
-        cin>>s;
-
-        for(int i=0; i<s.length(); i++){
-
-            if(s[i] == 'z')
-              count++;
+        for(int i=0; i<s.size(); i++){
+            
+            if(s[i] == 'z' || s[i]=='Z')
+              x++;
             
             else
-              min++;
+              y++;
 
         }
 
-        if(count == 2*min)
+        if(2*x == y)
            cout<<"Yes"<<endl;
         
         else
