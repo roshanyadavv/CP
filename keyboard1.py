d4119065c05eb2ab0dadc0199e387e57c8b5a3f8
Roshan Yadav <roshan.yadav2019@vitbhopal.ac.in>
2022-04-18 11:11:27 +0530

    cp

--- keyboard1.py
+++ keyboard1.py
@@ -1,6 +1,23 @@
 import keyboard
 keyboard.wait('1')
 a="""
-
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,i,j,k=0;
+cin>>n;
+int a[n][n];
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+cin>>a[i][j];
+if(a[i][j]<11){k=k+a[i][j];}
+}
+}
+cout<<k;
+return 0;
+}
 """
 keyboard.write(a)
